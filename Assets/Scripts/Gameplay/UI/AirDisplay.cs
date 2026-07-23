8e75059cfa0cd2b1fb2d92af01444480860fb5c8
Elijah Chapman <srdmstr@gmail.com>
2026-07-23 18:47:59 -0400

    Updated air UI to blend across color thresholds at different percent

--- Assets/Scripts/Gameplay/UI/AirDisplay.cs
+++ Assets/Scripts/Gameplay/UI/AirDisplay.cs
@@ -1,37 +1,81 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class AirDisplay : MonoBehaviour
 {
-    [SerializeField] Air playerAir;
-    [SerializeField] Image airFillImage;
-    [SerializeField] AirDisplayColors airDisplayColors;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Air playerAir;
+    [SerializeField] private Image airFillImage;
+    [SerializeField] private AirDisplayColors airDisplayColors;
+
+    private void Start()
     {
-        playerAir.EventAirChanged += UpdateAirVisual;
+        if (playerAir != null)
+        {
+            playerAir.EventAirChanged += UpdateAirVisual;
+        }
+
+        UpdateAirVisual();
     }
 
-    void UpdateAirVisual()
+    private void UpdateAirVisual()
     {
-        airFillImage.fillAmount = playerAir.GetAirPercent();
+        if (playerAir == null || airFillImage == null)
+        {
+            return;
+        }
+
+        float percent = Mathf.Clamp01(playerAir.GetAirPercent());
+        airFillImage.fillAmount = percent;
+        airFillImage.color = GetColorForPercent(percent);
+    }
+
+    private Color GetColorForPercent(float percent)
+    {
+        if (airDisplayColors.airDisplayColors == null || airDisplayColors.airDisplayColors.Count == 0)
+        {
+            return Color.white;
+        }
+
+        percent = Mathf.Clamp01(percent);
+
+        List<AirDisplayColor> colors = airDisplayColors.airDisplayColors;
+        colors.Sort((a, b) => a.percentThreshold.CompareTo(b.percentThreshold));
+
+        if (percent <= colors[0].percentThreshold)
+        {
+            return colors[0].color;
+        }
+
+        for (int i = 1; i < colors.Count; i++)
+        {
+            if (percent <= colors[i].percentThreshold)
+            {
+                float previousThreshold = colors[i - 1].percentThreshold;
+                float nextThreshold = colors[i].percentThreshold;
+
+                float t = Mathf.InverseLerp(previousThreshold, nextThreshold, percent);
+                t = Mathf.SmoothStep(0f, 1f, t);
+
+                return Color.Lerp(colors[i - 1].color, colors[i].color, t);
+            }
+        }
+
+        return colors[colors.Count - 1].color;
     }
 }
 
 [System.Serializable]
-struct AirDisplayColors
+public struct AirDisplayColors
 {
     public List<AirDisplayColor> airDisplayColors;
 }
 
 [System.Serializable]
-struct AirDisplayColor
+public struct AirDisplayColor
 {
     public string name;
-    public Color32 color;
+    public Color color;
     public float percentThreshold;
 }
